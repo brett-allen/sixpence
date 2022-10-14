f996c7b7907ef106490f4b519af4c75437e71cb1
brett <1888066+brett-allen@users.noreply.github.com>
2022-10-14 22:22:36 +1100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sixpence';
+  dataHolderBrands: any[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  onClick() {
+    console.log('onClick')
+    this.http.get('https://sherbetfizz.ddns.net:9462/cdr/data-holders').subscribe(
+      resp => {
+        (resp as any[]).forEach(element => {
+          this.dataHolderBrands.push(element)
+        });
+      }
+    )
+  }
 }
